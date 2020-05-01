864f6897c7d6ddb3ca37b2ffb7e96c71ead1a1b8
Jagdeep Virdi <virdijag@hotmail.com>
2020-05-01 10:25:17 +0100

    initial project commit

---
+++ src/build/precache-manifest.22b2a048213ad4341d7a84727b763b9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8327ff555b3cd9e88cde7bc7e99243a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61dc368d32dcff1c2ba9",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "19ddd23f1335f1d3ac7d",
+    "url": "/static/js/2.f7785ed8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f7785ed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61dc368d32dcff1c2ba9",
+    "url": "/static/js/main.843c5d13.chunk.js"
+  },
+  {
+    "revision": "c5d87880024193aa3633",
+    "url": "/static/js/runtime-main.56e1481a.js"
+  }
+]);
